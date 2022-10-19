17c7ff78b5f2b2788f2c3501c58d4a09fe35a2a7
Priya Yadav <91430096+Nishii4u@users.noreply.github.com>
2022-10-19 19:21:41 +0530

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int a,b;
+
+    printf("Enter 1st Number: ");
+    scanf("%d",&a);
+    printf("Enter 2nd Number: ");
+    scanf("%d",&b);
+    if(a==b)
+    printf("Equal");
+    else
+    printf("Not Equal ");
+    
+    
+}
